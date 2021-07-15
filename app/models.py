91508f774b08b97685a68dcac3529743294536f8
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-15 16:08:28 -0500

    added

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ STATIC_ROOT = '/tutorial/site/public/static'
 # Create your models here.
 class City(models.Model):
     name = models.CharField(max_length=25)
-    city_image = models.ImageField(upload_to=STATIC_ROOT, blank=True, null=True)
+    city_image = models.ImageField(upload_to='tutorial/site/public/static', blank=True, null=True)
     # we will see how this works 
 
     def __str__(self):
