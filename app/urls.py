84d288fdff0393a01e291d7ec2bc8fae3fa70982
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-15 16:33:06 -0500

    added

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns = patterns('',
-    # ... the rest of your URLconf goes here ...
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('', views.index, name='index'),
