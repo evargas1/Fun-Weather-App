10de303c03618cf8c6448ce9235c1fd75b459b3b
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-15 16:30:50 -0500

    added

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/tutorial/site/public/static'
 
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = '/tutorial/site/public/static/media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/tutorial/site/public/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = patterns('',
+    # ... the rest of your URLconf goes here ...
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
