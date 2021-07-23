8000dd0e5da6de0901292ba13473bdd754329cc8
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-23 11:31:43 -0500

    idiot mispelled something

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-app_name = 'app'
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.SearchResultsView, name='search'),
     path('prac/', views.prac, name='prac'),
-    path(r'^city_delete/(?P<id>\d+)/$', views.city_delete, name='city_delete'),
+    path('app/<int:id>/delete/', views.city_delete, name='city_delete'),
 ]
