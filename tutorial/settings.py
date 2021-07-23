57fec46689e9cba1e0d36ba0e71079e52e447a1f
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-23 18:05:50 -0500

    added

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = '/tutorial/site/public/static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/tutorial/site/public/media'
 
-
+FILE_UPLOAD_PERMISSIONS = 0o644
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
